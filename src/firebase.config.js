d35aebf0e42e108688b2fc156879212974763a56
Alex Solohub <le03grande@gmail.com>
2023-04-09 17:18:35 +0300

    dddddd

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -8,12 +8,12 @@ import {
 } from 'firebase/auth'
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // 'AIzaSyB_zcNt-6KZr4_gGAOdhgySmW4yq7svtkw'
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN, // 'test-crm-7afff.firebaseapp.com'
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID, // 'test-crm-7afff'
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET, // 'test-crm-7afff.appspot.com'
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID, // '897419797311'
-	appId: process.env.REACT_APP_FIREBASE_APP_ID, // '1:897419797311:web:8b1b1b1b1b1b1b1b1b1b1b1'
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY, 
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 const app = firebase.initializeApp(firebaseConfig)
 const auth = getAuth(app)
